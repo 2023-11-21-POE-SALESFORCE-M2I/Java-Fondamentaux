889666ee0662f32b8c359e3e1772d4a3a0c8993f
Twixy Twixy <twixy@free.fr>
2023-11-17 17:49:07 +0100

    preparations cours

---
+++ cours_exercices/exercices/streams/ExStream.java
@@ -0,0 +1,28 @@
+/**
+ * Exercices Stream
+ * 
+ * Creer 3 méthodes pour les choses suivantes puis executez les dans le programme principal.
+ * - Calcul et affiche le nombre de fois qu'un caractère apparait dans une chaine.
+ * - Filtre et affiche les mots commencant par une lettre donnée.
+ * - Converti et affiche une chaine representant une liste de nombres séparés par un ;
+ */ 
+package cours_exercices.exercices.streams;
+
+/**
+ * Exercice sur les streams
+ * @author Twixy
+ */
+public class ExStream {
+    // Calcul et affiche le nombre de fois qu'un caractère apparait dans une chaine.
+
+    // Filtre et affiche les mots commencant par une lettre donnée.
+    
+    // Converti et affiche une chaine representant une liste de nombres séparés par un ;
+        // "\\s+" : expression reguliere indiquant un espace ou plus.
+                // le :: remplace une lambda : mot -> ExStream.majusculeMot(mot).
+    
+    
+    public static void main(String[] args) {
+
+     }
+}
