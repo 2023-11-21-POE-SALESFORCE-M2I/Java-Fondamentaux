d3e45449abd08fd696c3ba8bb8a89e5a0a556566
Twixy Twixy <twixy@free.fr>
2023-12-21 15:43:25 +0100

    correction sur ennoce exercice stream

--- cours_exercices/exercices/streams/ExStream.java
+++ cours_exercices/exercices/streams/ExStream.java
@@ -1,25 +1,27 @@
 /**
  * Exercices Stream
  * 
- * Creer 3 méthodes pour les choses suivantes puis executez les dans le programme principal.
- * - Calcul et affiche le nombre de fois qu'un caractère apparait dans une chaine.
- * - Filtre et affiche les mots commencant par une lettre donnée.
- * - Converti et affiche une chaine representant une liste de nombres séparés par un ;
+ * Creer 3 mehodes pour les choses suivantes puis executez les dans le programme principal.
+ * - Calcul et affiche le nombre de fois qu'un caractere apparait dans une chaine.
+ * - Filtre et affiche les mots commencant par une lettre donnee.
+ * - Converti et affiche une chaine representant une liste de nombres separes par un ;
+ *   le resultat doit comporter ces nombres sur une longueur fixe de 4 caracteres et separer par une ,
+ * - Mettre en majuscule chaque mot dune phrase apres avoir supprime les espaces en trop dans celle-ci.
  */ 
 package cours_exercices.exercices.streams;
 
-/**
- * Exercice sur les streams
- * @author Twixy
- */
+// Classe ExStream
 public class ExStream {
-    // Calcul et affiche le nombre de fois qu'un caractère apparait dans une chaine.
+    // Calcul et affiche le nombre de fois qu'un caractere apparait dans une chaine.
 
-    // Filtre et affiche les mots commencant par une lettre donnée.
+    // Filtre et affiche les mots commencant par une lettre donnee.
     
-    // Converti et affiche une chaine representant une liste de nombres séparés par un ;
-        // "\\s+" : expression reguliere indiquant un espace ou plus.
-                // le :: remplace une lambda : mot -> ExStream.majusculeMot(mot).
+    // Converti et affiche une chaine representant une liste de nombres separe par un ;
+    // le resultat doit comporter ces nombres sur une longueur fixe de 4 caracteres et separer par une ,
+
+    // Met en majscule la premiere lettre d'un mot.
+
+    // Met en majscule chaque mot dune phrase apres avoirs supprime les espaces en trop dans celle-ci.
     
     
     public static void main(String[] args) {
