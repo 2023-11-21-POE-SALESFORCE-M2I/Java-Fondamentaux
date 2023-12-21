a8909cb689aeb1abe438726985ede4b6dfad591c
Twixy Twixy <twixy@free.fr>
2023-12-21 08:43:20 +0100

    ajout exercice enumerations

---
+++ cours_exercices/exercices/enumerations/Enumerations.java
@@ -0,0 +1,29 @@
+/*
+ * Exercice enumeration
+ * Declarer une enumeration appelee GenreLitteraire avec les valeurs suivantes : 
+ * ROMAN, SCIENCE_FICTION, POESIE, THRILLER, HISTOIRE
+ * Creer une classe appelee Livre avec les attributs suivants :
+ *      - titre
+ *      - auteur
+ *      - genre
+ * Instancier quelques objets de la classe Livre avec des valeurs fictives.
+ * Afficher les details de chaque livre, y compris le titre, l'auteur et le genre.
+ */
+package cours_exercices.exercices.enumerations;
+
+// Classe GenreLitteraire de type enum
+
+// Classe Livre
+
+    // Constructeur
+
+    // Methode afficherDetails
+
+// Classe Enumeration
+public class Enumerations {
+    public static void main(String[] args) {
+        // Creation des objets Livre
+
+        // Affichage des livres
+    }    
+}
