c6bbcaf2928a0645393c665b94d28cd865238037
Twixy Twixy <twixy@free.fr>
2024-01-04 17:16:24 +0100

    projet JDBC

---
+++ corriges/exercices/JDBC/Solution1/interfaces/IConstantes.java
@@ -0,0 +1,14 @@
+package corriges.exercices.JDBC.Solution1.interfaces;
+
+/**
+ * Constantes
+ */
+/*
+public interface IConstantes {
+    static final String NOM_BDD = "MaBDD";
+    static final String NOM_TABLE_U = "Utilisateurs";
+    static final String NOM_TABLE_C = "Clients";
+    static final String NOM_TABLE_F = "Fournisseurs";
+    static final String NOM_TABLE_A = "Articles";
+}
+*/
